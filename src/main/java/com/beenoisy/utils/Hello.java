9da2c47a3a4f5238d35283563a34bebd1d022a53
jacks808 <jacks808@163.com>
2014-10-24 14:19:08 +0800

    commit HelloWorld Class
    Change-Id: Ia1003bf95e025d9d2bff91588f9bbd02375b35b1
    Signed-off-by: jacks808 <jacks808@163.com>

---
+++ src/main/java/com/beenoisy/utils/Hello.java
@@ -0,0 +1,38 @@
+/*
+ * beenoisy-utils - com.beenoisy.utils.Hello.java
+ * 2014年10月24日:下午2:09:13
+ * Keen
+ *
+ * jacks808@163.com
+ *
+ * KKKKKKKKK    KKKKKKK                                                          
+ * K:::::::K    K:::::K                                                          
+ * K:::::::K    K:::::K                                                          
+ * K:::::::K   K::::::K                                                          
+ * KK::::::K  K:::::KKK    eeeeeeeeeeee        eeeeeeeeeeee    nnnn  nnnnnnnn    
+ *   K:::::K K:::::K     ee::::::::::::ee    ee::::::::::::ee  n:::nn::::::::nn  
+ *   K::::::K:::::K     e::::::eeeee:::::ee e::::::eeeee:::::een::::::::::::::nn 
+ *   K:::::::::::K     e::::::e     e:::::ee::::::e     e:::::enn:::::::::::::::n
+ *   K:::::::::::K     e:::::::eeeee::::::ee:::::::eeeee::::::e  n:::::nnnn:::::n
+ *   K::::::K:::::K    e:::::::::::::::::e e:::::::::::::::::e   n::::n    n::::n
+ *   K:::::K K:::::K   e::::::eeeeeeeeeee  e::::::eeeeeeeeeee    n::::n    n::::n
+ * KK::::::K  K:::::KKKe:::::::e           e:::::::e             n::::n    n::::n
+ * K:::::::K   K::::::Ke::::::::e          e::::::::e            n::::n    n::::n
+ * K:::::::K    K:::::K e::::::::eeeeeeee   e::::::::eeeeeeee    n::::n    n::::n
+ * K:::::::K    K:::::K  ee:::::::::::::e    ee:::::::::::::e    n::::n    n::::n
+ * KKKKKKKKK    KKKKKKK    eeeeeeeeeeeeee      eeeeeeeeeeeeee    nnnnnn    nnnnnn
+ * 
+ */
+package com.beenoisy.utils;
+
+/**
+ * beenoisy util hello world <br>
+ * 2014年10月24日:下午2:09:13
+ * 
+ * @author Keen | ziyi.wang@renren-inc.com <br>
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
